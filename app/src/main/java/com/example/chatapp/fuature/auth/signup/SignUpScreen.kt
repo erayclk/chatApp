e93c4b8971cd4601c7c18d8db5e335991e87bfd3
erayclk <92385578+erayclk@users.noreply.github.com>
2025-05-22 22:47:30 +0300

    fix sign-in success state handling and improve layout spacing in sign-in screen

--- app/src/main/java/com/example/chatapp/fuature/auth/signup/SignUpScreen.kt
+++ app/src/main/java/com/example/chatapp/fuature/auth/signup/SignUpScreen.kt
@@ -1,36 +1,66 @@
 package com.example.chatapp.fuature.auth.signup
 
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.example.chatapp.R
 
 
 @Composable
-fun SignUpScreen(navController: NavController){
+fun SignUpScreen(navController: NavController) {
+
+    val viewModel: SignUpScreenViewModel = hiltViewModel()
+    val uiState = viewModel.state.collectAsState()
+
+    val context = LocalContext.current
+
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     var fullName by remember { mutableStateOf("") }
     var confirmPassword by remember { mutableStateOf("") }
+
+    LaunchedEffect(key1 = uiState.value) {
+        when (uiState.value) {
+            is SignUpState.Success -> {
+                navController.navigate("HomeScreen")
+            }
+
+            is SignUpState.Error -> {
+                Toast.makeText(context, "Sign Up Failed ${email}*", Toast.LENGTH_LONG).show()
+            }
+
+            else -> {}
+        }
+    }
+
+
+
 
     Scaffold(modifier = Modifier.fillMaxSize()) {
         Column(
@@ -66,7 +96,7 @@ fun SignUpScreen(navController: NavController){
                 label = { Text("Password") },
                 modifier = Modifier.fillMaxWidth(),
 
-            )
+                )
             OutlinedTextField(
                 value = confirmPassword,
                 onValueChange = { confirmPassword = it },
@@ -74,21 +104,32 @@ fun SignUpScreen(navController: NavController){
                 modifier = Modifier.fillMaxWidth(),
                 isError = password != confirmPassword,
 
-            )
-            Button(
-                onClick = { /*TODO*/ },
-                enabled = fullName.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty() && confirmPassword.isNotEmpty(),
-                modifier = Modifier
+                )
+            Spacer(Modifier.padding(16.dp))
+            if (uiState.value == SignUpState.Loading) {
+                CircularProgressIndicator(
 
-                    .fillMaxWidth()
-            ) {
-                Text("Sign In")
-            }
-            TextButton(
+                )
 
-                onClick = { navController.navigate("singin") }
-            ) {
-                Text("if you don't have an account, Sign Up")
+
+            } else {
+                Button(
+                    onClick = {
+                        viewModel.signUp(fullName,email, password, )
+                    },
+                    enabled = fullName.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty() && confirmPassword.isNotEmpty(),
+                    modifier = Modifier
+
+                        .fillMaxWidth()
+                ) {
+                    Text("Sign In")
+                }
+                TextButton(
+
+                    onClick = { navController.navigate("singin") }
+                ) {
+                    Text("if you don't have an account, Sign Up")
+                }
             }
 
 
@@ -96,5 +137,5 @@ fun SignUpScreen(navController: NavController){
 
     }
 
-    
+
 }
--- app/src/main/java/com/example/chatapp/fuature/auth/signup/SignUpScreenViewModel.kt
+++ app/src/main/java/com/example/chatapp/fuature/auth/signup/SignUpScreenViewModel.kt
@@ -2,7 +2,48 @@ package com.example.chatapp.fuature.auth.signup
 
 import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.UserProfileChangeRequest
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import javax.inject.Inject
 
+
+@HiltViewModel
+class SignUpScreenViewModel @Inject constructor() : ViewModel() {
+    val _state = MutableStateFlow<SignUpState>(SignUpState.Nothing)
+    val state = _state.asStateFlow()
+
+    fun signUp(name: String, email: String, password: String) {
+        _state.value = SignUpState.Loading
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    task.result.user?.let {
+                        it.updateProfile(
+                            UserProfileChangeRequest.Builder()
+                                .setDisplayName(name)
+                                .build()
+                        ).addOnCompleteListener {
+                            _state.value = SignUpState.Success
+                        }
+                    }
+                    _state.value = SignUpState.Success
+                } else {
+                    _state.value = SignUpState.Error
+                }
+            }
+
+
+    }
+
+}
+
+sealed class SignUpState {
+    object Nothing : SignUpState()
+    object Loading : SignUpState()
+    object Success : SignUpState()
+    object Error : SignUpState()
+
+}
