04844678dcbf58315dad761ced28932033410c0a
erayclk <92385578+erayclk@users.noreply.github.com>
2025-05-23 22:52:24 +0300

    add chat screen and navigation argument for channel id

--- app/src/main/java/com/example/chatapp/MainScreen.kt
+++ app/src/main/java/com/example/chatapp/MainScreen.kt
@@ -4,11 +4,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.chatapp.fuature.auth.signin.SignInScreen
 import com.example.chatapp.fuature.auth.signup.SignUpScreen
+import com.example.chatapp.fuature.chat.ChatScreen
 import com.example.chatapp.fuature.home.HomeScreen
 import com.google.firebase.auth.FirebaseAuth
 
@@ -34,6 +37,18 @@ fun MainScreen() {
             }
             composable("HomeScreen") {
                 HomeScreen(navController = navController)
+            }
+            composable(
+                "chat/{channelId}", arguments =
+                listOf(
+                    navArgument("channelId") {
+                        type = NavType.StringType
+                    })
+            ) {
+                val channelId = it.arguments?.getString("channelId") ?: ""
+                ChatScreen(
+                    navController = navController, channelId
+                )
             }
 
 
---
+++ app/src/main/java/com/example/chatapp/fuature/chat/ChatScreenViewModel.kt
@@ -0,0 +1,61 @@
+package com.example.chatapp.fuature.chat
+
+import androidx.compose.runtime.collectAsState
+import androidx.lifecycle.ViewModel
+import com.example.chatapp.model.Message
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.database
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import java.util.UUID
+import javax.inject.Inject
+
+@HiltViewModel
+class ChatScreenViewModel @Inject constructor(): ViewModel() {
+
+    private val _messages = MutableStateFlow<List<Message>>(emptyList())
+    val messages = _messages.asStateFlow()
+    val db = Firebase.database
+
+
+    fun sendMessage(channelID: String, messageText: String) {
+        val message = com.example.chatapp.model.Message(
+            db.reference.push().key ?: UUID.randomUUID().toString(),
+            Firebase.auth.currentUser?.uid ?: "",
+            messageText,
+            System.currentTimeMillis(),
+            Firebase.auth.currentUser?.displayName ?: "",
+            null,
+            null
+        )
+
+        db.reference.child("messages").child(channelID).push().setValue(message)
+    }
+    fun listenForMessages(channelID: String) {
+        db.getReference("messages").child(channelID).orderByChild("createdAt")
+            .addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val list = mutableListOf<Message>()
+                    snapshot.children.forEach { data ->
+                        val message = data.getValue(Message::class.java)
+                        message?.let {
+                            list.add(it)
+                        }
+                    }
+                    _messages.value = list
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    // Handle error
+                }
+            })
+    }
+
+
+
+}
